31afce5df45fc4bbfa03040e5d826670fc7cfc46
Nirmal <nirmaljohnsonk8@gmail.com>
2024-05-23 20:31:13 +0530

    Initial commit with basic project structure

---
+++ src/test/java/com/nirmaljohnson/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirmaljohnson.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
