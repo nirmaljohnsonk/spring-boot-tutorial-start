cf6008308aded3e3f634fd2037425af3b297e1f3
Nirmal <nirmaljohnsonk8@gmail.com>
2024-05-24 13:30:18 +0530

    Basic classes added

---
+++ src/main/java/com/nirmaljohnson/runnerz/Application.java
@@ -0,0 +1,33 @@
+package com.nirmaljohnson.runnerz;
+
+import com.nirmaljohnson.runnerz.run.Location;
+import com.nirmaljohnson.runnerz.run.Run;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner test(){
+		return args->{
+			Run run = new Run(1,"First Run", LocalDateTime.now(), LocalDateTime.now().plusMinutes(20),15, Location.INDOOR);
+			log.info("Run : {}", run);
+		};
+	}
+
+}
---
+++ src/main/java/com/nirmaljohnson/runnerz/run/Location.java
@@ -0,0 +1,5 @@
+package com.nirmaljohnson.runnerz.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
